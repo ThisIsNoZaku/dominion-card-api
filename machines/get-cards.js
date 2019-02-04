a429546b5e9e1008e29a271db7f570a54f1962f7
Damien Marble <damienmarble@gmail.com>
2019-02-04 04:29:27 +0000

    Initial commit.

---
+++ machines/get-cards.js
@@ -0,0 +1,41 @@
+const cards = JSON.parse(require("fs").readFileSync(require("path").resolve(__dirname, "../cards.json")));
+
+module.exports = {
+
+
+  friendlyName: 'Get cards',
+
+
+  description: 'Get cards with optional filters',
+
+
+  cacheable: false,
+
+
+  sync: true,
+
+
+  inputs: {
+
+  },
+
+
+  exits: {
+
+    success: {
+      variableName: 'result',
+      description: 'Done.',
+    },
+
+  },
+
+
+  fn: function(inputs, exits
+    /*``*/
+  ) {
+    return exits.success(cards);
+  },
+
+
+
+};
