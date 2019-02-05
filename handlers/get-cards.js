41934ecfbf51a2ff7bdeb5145644eb996a3d59a2
Damien Marble <damienmarble@gmail.com>
2019-02-05 23:03:16 +0000

    Added passing query strings to get-cards.

--- handlers/get-cards.js
+++ handlers/get-cards.js
@@ -2,10 +2,16 @@ const machine = require("../index.js");
 
 exports.fn = async function(event, context, callback){
     try {
-        const cards = machine.getCards().now();
+        const cards = machine.getCards({
+            sets: event.queryStringParameters.sets ? 
+                event.queryStringParameters.sets.split(",") : undefined,
+            types: event.queryStringParameters.types ? 
+                event.queryStringParameters.types.split(",") : undefined
+        }).now();
+        console.log(event);
         callback(null, {
             status: 200,
-            body: JSON.stringify(cards)
+            body: JSON.stringify({cards})
         });
     } catch(e) {
         callback(e);
